2c433a680efa9f513c9da9b5ecab20497828e109
danilbushkov <danil1357997@gmail.com>
2024-03-17 16:44:03 +0300

    add setContext, getContext, getStep and setStep to Storage

--- internal/bot/storage.go
+++ internal/bot/storage.go
@@ -53,23 +53,78 @@ func (s *Storage) components(botId int64, groupId int64) (map[int64]*model.Compo
 }
 
 func (s *Storage) groupId() int64 {
-	return 1
+	return config.MainGroupId
 }
 
-func (s *Storage) setContext(userId int64, botId int64, groupId int64, ctx *context.Context) {
-
+func (s *Storage) setContext(botId int64, groupId int64, userId int64, ctx *context.Context) error {
+	err := s.db.SetUserContextByTgId(botId, userId, ctx)
+	if err != nil {
+		return err
+	}
+	err = s.redis.SetUserContext(botId, groupId, userId, ctx)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func (s *Storage) context(userId int64) {
+func (s *Storage) context(botId int64, groupId int64, userId int64) (*context.Context, error) {
+	ctx, err := s.redis.GetUserContext(botId, groupId, userId)
+	if err != nil {
+		return nil, err
+	}
+	if ctx == nil {
+		ctx, err = s.db.UserContextByTgId(botId, userId)
+		if err != nil {
+			return nil, err
+		}
+		if ctx != nil {
+			err = s.redis.SetUserContext(botId, groupId, userId, ctx)
+			if err != nil {
+				return nil, err
+			}
+		}
+		s.log.Debugw("get user context from db", "botId", botId, "userId", userId)
+		return ctx, nil
+	}
 
+	s.log.Debugw("get user context from redis", "botId", botId, "userId", userId)
+	return ctx, nil
 }
 
-func (s *Storage) userStep() {
+func (s *Storage) userStep(botId int64, groupId int64, userId int64) (int64, error) {
+	step, err := s.redis.GetUserStep(botId, groupId, userId)
+	if err != nil {
+		return 0, err
+	}
+	if step == nil {
+		step, err := s.db.UserStepByTgId(botId, userId)
+		if err != nil {
+			return 0, err
+		}
+		err = s.redis.SetUserStep(botId, groupId, userId, step)
+		if err != nil {
+			return 0, err
+		}
 
+		s.log.Debugw("get user step from db", "botId", botId, "userId", userId)
+		return step, nil
+	}
+
+	s.log.Debugw("get user step from redis", "botId", botId, "userId", userId)
+	return *step, nil
 }
 
-func (s *Storage) setUserStep() {
-
+func (s *Storage) setUserStep(botId int64, groupId int64, userId int64, stepId int64) error {
+	err := s.db.SetUserStepByTgId(botId, userId, stepId)
+	if err != nil {
+		return err
+	}
+	err = s.redis.SetUserStep(botId, groupId, userId, stepId)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (s *Storage) addUser(botId int64, from *telego.User) error {
--- internal/database/redis/user.go
+++ internal/database/redis/user.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"strconv"
 
+	bctx "github.com/botscubes/bot-components/context"
 	"github.com/botscubes/bot-worker/internal/config"
 	"github.com/redis/go-redis/v9"
 )
@@ -41,6 +42,42 @@ func (rdb *Rdb) GetUserStep(botId int64, groupId int64, userID int64) (*int64, e
 	}
 
 	return &stepID, nil
+}
+func (rdb *Rdb) SetUserContext(botId int64, groupId int64, userId int64, bctx *bctx.Context) error {
+	ctx := context.Background()
+
+	key := "user:" + strconv.FormatInt(userId, 10) +
+		":bot:" + strconv.FormatInt(botId, 10)
+
+	field := "group:" + strconv.FormatInt(groupId, 10) + ":context"
+	bytes, err := bctx.ToJSON()
+	if err != nil {
+		return err
+	}
+	if err := rdb.HSet(ctx, key, field, bytes).Err(); err != nil {
+		return err
+	}
+
+	return rdb.Expire(ctx, key, config.RedisExpire).Err()
+}
+
+func (rdb *Rdb) GetUserContext(botId int64, groupId int64, userId int64) (*bctx.Context, error) {
+	ctx := context.Background()
+
+	key := "user:" + strconv.FormatInt(userId, 10) +
+		":bot:" + strconv.FormatInt(botId, 10)
+
+	field := "group:" + strconv.FormatInt(groupId, 10) + ":context"
+
+	bytes, err := rdb.HGet(ctx, key, field).Bytes()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return bctx.NewContextFromJSON(bytes)
 }
 
 func (rdb *Rdb) CheckUserExist(botId int64, userID int64) (int64, error) {
